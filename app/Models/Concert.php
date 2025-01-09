598ee20f8002c9ffdcc25b0de5d5e5c9a8f0c4df
Rogerjove <rogerjove2005@gmail.com>
2025-01-09 16:41:59 +0100

    feat(migrations): create Concert Models (class Concert extended of Model) Added a migration to create the pivot table for the many-to-many relationship between bands and concerts. The table includes: - id

--- app/Models/Concert.php
+++ app/Models/Concert.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Concert extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'start_datetime', 'end_datetime', 'max_people', 'tickets_sold'
+    ];
+
+    public function bands()
+    {
+        return $this->belongsToMany(Band::class, 'band_concert');
+    }
 }
+
